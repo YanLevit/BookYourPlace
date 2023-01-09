59260c1b7e9322e1fededc6b25ac00fd6479f9ef
SemionSolonar <shimon22361@cs.colman.ac.il>
2023-01-09 17:54:52 +0200

    Connect firebase to project

--- app/src/main/java/com/example/bookyourplace/MainActivity.java
+++ app/src/main/java/com/example/bookyourplace/MainActivity.java
@@ -1,8 +1,18 @@
 package com.example.bookyourplace;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
     }
 }
